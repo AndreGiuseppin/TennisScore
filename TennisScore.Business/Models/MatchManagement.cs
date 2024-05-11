3d51e2136da12da80b51701c19eb9ddd5e90bb59
André Nascimento Giuseppin <andre.giuseppin@gmail.com>
2024-05-11 15:05:15 -0300

    add matchManagement interface add service to choice set

---
+++ TennisScore.Business/Models/MatchManagement.cs
@@ -0,0 +1,9 @@
+﻿namespace TennisScore.Business.Models
+{
+    public class MatchManagement
+    {
+        public int TotalSets { get; private set; }
+
+        public void WithSets(int set) => TotalSets = set;
+    }
+}
